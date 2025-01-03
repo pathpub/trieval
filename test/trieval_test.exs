33fd6e85e7b08558a9f17148f5961eca8f025acb
Adam <adam.w.mark@gmail.com>
2025-01-03 16:16:04 -0500

    format files using mix format

--- test/trieval_test.exs
+++ test/trieval_test.exs
@@ -9,31 +9,35 @@ defmodule TrievalTest do
 
   @test_trie Trieval.new(@test_data)
 
-  @payload_trie Enum.map(@test_data, fn item = <<first, _ :: binary>> ->
-    {item, <<first>>}
-  end) |> Trieval.new
-
+  @payload_trie Enum.map(@test_data, fn item = <<first, _::binary>> ->
+                  {item, <<first>>}
+                end)
+                |> Trieval.new()
 
   test "empty trie" do
-    assert Trieval.new == %Trieval.Trie{}
+    assert Trieval.new() == %Trieval.Trie{}
   end
 
   test "contains?" do
     assert Trieval.contains?(@test_trie, "apple") == true
     assert Trieval.contains?(@test_trie, "smash") == true
     assert Trieval.contains?(@test_trie, "abcde") == false
-    assert Trieval.contains?(@test_trie, "app")   == false
+    assert Trieval.contains?(@test_trie, "app") == false
   end
 
   test "prefix" do
     assert Trieval.prefix(@test_trie, "app") == ["apple", "apply"]
-    assert Trieval.prefix(@test_trie, "n")   == ["negative"]
+    assert Trieval.prefix(@test_trie, "n") == ["negative"]
     assert Trieval.prefix(@test_trie, "abc") == []
   end
 
   test "longest_common_prefix" do
-    assert Trieval.longest_common_prefix(@test_trie, "fr") == {"frie", ["fried", "friendly", "friend", "frieze"]}
-    assert Trieval.longest_common_prefix(@test_trie, "frien") == {"friend", ["friendly", "friend"]}
+    assert Trieval.longest_common_prefix(@test_trie, "fr") ==
+             {"frie", ["fried", "friendly", "friend", "frieze"]}
+
+    assert Trieval.longest_common_prefix(@test_trie, "frien") ==
+             {"friend", ["friendly", "friend"]}
+
     assert Trieval.longest_common_prefix(@test_trie, "winter") == {"winter", ["winter"]}
     assert Trieval.longest_common_prefix(@test_trie, "abc") == {nil, []}
   end
@@ -55,7 +59,7 @@ defmodule TrievalTest do
 
   test "payload prefix" do
     assert Trieval.prefix(@payload_trie, "app") == [{"apple", "a"}, {"apply", "a"}]
-    assert Trieval.prefix(@payload_trie, "n")   == [{"negative", "n"}]
+    assert Trieval.prefix(@payload_trie, "n") == [{"negative", "n"}]
     assert Trieval.prefix(@payload_trie, "abc") == []
   end
 
@@ -65,5 +69,4 @@ defmodule TrievalTest do
     assert Trieval.pattern(@payload_trie, "[co]**") == [{"cat", "c"}, {"out", "o"}]
     assert Trieval.pattern(@payload_trie, "{1[^okjh]}x[tnm]{1}*{2}{1}{2}") == [{"extended", "e"}]
   end
-
 end
